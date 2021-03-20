feb3f9f35ab1276ff56f09d864a1b24134250c6a
hex <hex>
2021-03-19 21:46:02 -0500

    added new build with apis

---
+++ build/precache-manifest.56aa1924ef473f99c2e630add7fe32c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fb1ccd2e6fc5dca9623",
+    "url": "/static/css/main.9c580d30.chunk.css"
+  },
+  {
+    "revision": "5fb1ccd2e6fc5dca9623",
+    "url": "/static/js/main.5fb1ccd2.chunk.js"
+  },
+  {
+    "revision": "5c86b34d3f1e9c3d2203",
+    "url": "/static/js/1.5c86b34d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "91f2c37edebe51c4ff0d5051afad7348",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.477032da76dd58f3890fbad7a8baf03f.js"
+  "/precache-manifest.56aa1924ef473f99c2e630add7fe32c7.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
